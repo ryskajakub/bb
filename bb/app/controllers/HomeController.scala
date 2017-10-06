59a8a353c7fbd789f8c638aac85fa6ae1dec7fe4
Jakub Ryška <ryskajakub@seznam.cz>
2017-10-06 14:53:44 +0200

    status, push, receive routes

--- bb/app/controllers/HomeController.scala
+++ bb/app/controllers/HomeController.scala
@@ -4,9 +4,13 @@ import play.api._
 import play.api.mvc._
 import play.api.libs.json._
 
+import akka.stream.scaladsl.Source
+
 import javax.inject._
 import java.util.UUID
 import java.util.concurrent.ForkJoinPool
+import java.text.SimpleDateFormat
+import java.util.Date
 
 import scala.concurrent.{Future, Promise, ExecutionContext}
 
@@ -65,12 +69,16 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
     Ok(views.html.index())
   }
 
+  val messageDateFormat = 
+    new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ")
+
   def mkMessage () = {
     val uuid = UUID.randomUUID().toString()
+    val date = messageDateFormat.format(new Date)
     val messageBody = JsObject (
       Map (
-        ("uuid", JsString(uuid)),
-        ("createdAt", JsString (""))
+        ("uuid", JsString(uuid)) ,
+        ("createdAt", JsString(date))
         )
       )
     new SendMessageBatchRequestEntry(
@@ -78,14 +86,14 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
       Json.stringify(messageBody));
   }
 
-  val batchSize = 2
-  val batchesCount = 2
+  val writeBatchSize = 10   
+  val writeBatchesCount = 1000 
 
   def pushMessages() = Action.async {
 
-    val allRequests = (0 until batchesCount).map(_ => {
+    val allRequests = (0 until writeBatchesCount).map(_ => {
       val p = Promise[Unit]()
-      val entries = (0 until batchSize).map(_ => mkMessage)
+      val entries = (0 until writeBatchSize).map(_ => mkMessage)
       val request = new SendMessageBatchRequest().withQueueUrl(queueUrl).withEntries(JavaConverters.asJavaCollection(entries))
       val handler = new AsyncHandler[SendMessageBatchRequest,
           SendMessageBatchResult] {
@@ -123,40 +131,65 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
     val (f, handler) = mkHandler[GetQueueAttributesRequest, 
       GetQueueAttributesResult]()
     sqs.getQueueAttributesAsync(queueUrl, messageTypes, handler)
-    f.map{x => System.out.println(x.getAttributes); Ok("ok")}(awsEc)
+    f.map{x => 
+      (JavaConverters.mapAsScalaMap
+          (x.getAttributes).get(messageType)) match {
+        case Some(y) => Ok(y)
+        case None => InternalServerError
+      }
+    }(awsEc)
   }
 
-  def readMessages() = Action.async {
+  val runs = 20
+  val readBatchSize = 5000
 
-    def readMessage(result: ReceiveMessageResult): Unit = {
-      JavaConverters.asScalaBuffer(result.getMessages()).foreach{m =>
-        System.out.println(m.getBody())
+  def readMessages() = Action {
+
+    def readMessage(countdown: Int)
+        (messages: Seq[Message]): 
+        Option[(Int,String)] = {
+      countdown match {
+        case 0 => None
+        case _ => {
+          val messagesLength = messages.length
+          val beginning = (countdown, messagesLength) match {
+            case (`runs`, _) => "["
+            case (_, 0) => ""
+            case (1, _) => ","
+            case _ => ","
+          }
+          val end = (countdown, messagesLength) match {
+            case (_, 0) => "]"
+            case (1, _) => "]"
+            case _ => ""
+          }
+          val asJson = messages.map(_.getBody()).mkString(",")
+          val newState = if (messagesLength == 0) { -1 } 
+              else { countdown - 1 }  
+          Some(newState, beginning + asJson + end)
+        }
       }
     }
     
-    def receiveMessage: Future[ReceiveMessageResult] = {
-      val p = Promise[ReceiveMessageResult]()
+    def receiveMessage(countdown: Int): 
+        Future[Option[(Int, String)]] = {
       val request = new ReceiveMessageRequest(queueUrl)
-      val handler = new AsyncHandler[ReceiveMessageRequest,
-          ReceiveMessageResult] {
-        def onError(e: Exception) = p.failure(e)
-        def onSuccess(req: ReceiveMessageRequest, 
-            res: ReceiveMessageResult) = p.success(res)
-      }
-      sqs.receiveMessageAsync(request, handler)
-      p.future
+        .withMaxNumberOfMessages(10)
+      val sentRequests: Seq[Future[ReceiveMessageResult]] = ((0 until readBatchSize).map { _ =>
+        val (f, handler) = 
+          mkHandler[ReceiveMessageRequest, ReceiveMessageResult] ()
+        sqs.receiveMessageAsync(request, handler)
+        f
+      })
+      implicit val c = awsEc
+      val batchedSentRequests:Future[Seq[Message]] = 
+        Future.sequence(sentRequests).map(_.flatMap(x =>
+          JavaConverters.asScalaBuffer(x.getMessages())))
+      batchedSentRequests.map(readMessage(countdown))(awsEc)
     }
 
-    receiveMessage.map{m => readMessage(m); Ok("ok")}(awsEc)
-
-    /*
-    def repeatTillEnd () = {
-      receiveMessage.flatMap { rmr =>
-        
-      }
-      repeatTillEnd
-    }
-    */
+    val source = Source.unfoldAsync(runs)(x => receiveMessage(x))
+    Ok.chunked(source).as("application/json")
 
   }
 
